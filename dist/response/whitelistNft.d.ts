6d319d26b0ed0c20090089a7d83aa0b27afbf077
dong <nhnguhoc@gmail.com>
2022-04-13 17:18:46 +0700

    feat/LIQ-450 update whitelistNft response schema

--- dist/response/whitelistNft.d.ts
+++ dist/response/whitelistNft.d.ts
@@ -1,6 +1,7 @@
 import { NftCreator } from "../nft";
 export interface WhitelistNft {
     candyShopAddress: string;
+    tokenMint: string;
     creators: NftCreator[];
     symbol: string;
     identifier: string;
--- src/response/whitelistNft.ts
+++ src/response/whitelistNft.ts
@@ -2,6 +2,7 @@ import { NftCreator } from "../nft";
 
 export interface WhitelistNft {
   candyShopAddress: string;
+  tokenMint: string;
   creators: NftCreator[];
   symbol: string;
   identifier: string;
